3ced9fedd90177c77d7188857fc28d7d015862b3
Roberto Còntini <roberto.contini@abstract.it>
2026-02-23 09:42:17 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <main id="main-content" tabIndex={-1}>
           {children}
         </main>
+        <Analytics />
       </body>
     </html>
   )
